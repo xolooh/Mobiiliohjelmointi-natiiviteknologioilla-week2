81f8454a849640c6d1e2358765d1a424a04dcf13
xolooh <tapsukk@hotmail.fi>
2024-09-03 08:50:26 +0300

    ready

---
+++ app/src/main/java/com/example/weeklyassignment2/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.weeklyassignment2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.weeklyassignment2.ui.theme.WeeklyAssignment2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeeklyAssignment2Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Bmi()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Bmi() {
+    var heightInput by remember { mutableStateOf("") }
+    var weightInput by remember { mutableStateOf("") }
+    val height = heightInput.toFloatOrNull() ?: 0.0f
+    val weight = weightInput.toIntOrNull() ?: 0
+    val bmi = if (weight > 0 && height > 0) weight / (height * height) else 0.0
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = "Body mass index",
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.primary,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth().padding(top = 16.dp, bottom = 16.dp)
+        )
+        OutlinedTextField(
+            value = heightInput,
+            onValueChange = { heightInput = it.replace(',', '.') },
+            label = { Text("Height (m)") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = weightInput,
+            onValueChange = { weightInput = it.replace(',', '.') },
+            label = { Text("Weight (kg)") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = "Your BMI is: ${String.format("%.2f", bmi).replace(',', '.')}",
+
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WeeklyAssignment2Theme {
+        Bmi()
+    }
+}
